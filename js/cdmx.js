253c90c9b0a685881bf304773ad6b6ac4b0e52e2
Diego Garcia Padilla <dgarcia144@alumnos.uaq.mx>
2021-12-19 15:17:54 -0600

    aumentar rango

--- js/cdmx.js
+++ js/cdmx.js
@@ -1,5 +1,5 @@
 //The maximum zoom level to cluster data point data on the map.
-var maxClusterZoomLevel = 1;
+var maxClusterZoomLevel = 8;
 
 //The URL to the store location data.
 var storeLocationDataUrl = 'Coordenadas/CDMX/CDMXCoor.txt';
