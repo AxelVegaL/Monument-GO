d23c3d43773a260b3976b892e83627f4b3a97fcc
Diego Garcia Padilla <dgarcia144@alumnos.uaq.mx>
2021-12-18 21:10:24 -0600

    Update cdmx.js

--- js/cdmx.js
+++ js/cdmx.js
@@ -2,10 +2,10 @@
 var maxClusterZoomLevel = 11;
 
 //The URL to the store location data.
-var storeLocationDataUrl = '../Coordenadas\CDMX\CDMXCoor.txt';
+var storeLocationDataUrl = '../Coordenadas/CDMX/CDMXCoor.txt';
 
 //The URL to the icon image.
-var iconImageUrl = 'media\pin.png';
+var iconImageUrl = '../media/pin.png';
 var map, popup, datasource, iconLayer, centerMarker, searchURL;
 
 //---------------------------------------------------------------
