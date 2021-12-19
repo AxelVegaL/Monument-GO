243e55b1a44c5bef47444ca8128de6b66bd2a57b
Diego Garcia Padilla <dgarcia144@alumnos.uaq.mx>
2021-12-18 22:07:46 -0600

    web

--- js/cdmx.js
+++ js/cdmx.js
@@ -1,5 +1,5 @@
 //The maximum zoom level to cluster data point data on the map.
-var maxClusterZoomLevel = 11;
+var maxClusterZoomLevel = 100;
 
 //The URL to the store location data.
 var storeLocationDataUrl = 'Coordenadas/CDMX/CDMXCoor.txt';
