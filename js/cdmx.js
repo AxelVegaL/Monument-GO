093a7bc5e0eb9e51206bfb87b97512c0cbd6a04e
SERGIOE2112 <inv3797@innovaccion.mx>
2021-12-19 14:47:00 -0600

    add orange point and zoom up and zoom down

--- js/cdmx.js
+++ js/cdmx.js
@@ -52,9 +52,107 @@ function initialize() {
     document.getElementById('myLocationBtn').onclick = setMapToUserLocation;
 
     //Wait until the map resources are ready.
-    map.events.add('ready', function() {
+        //Add the zoom control to the map.
+        map.controls.add(new atlas.control.ZoomControl(), {
+            position: 'top-right'
+        });
 
-        //Add your post-map load functionality.
+        //Add an HTML marker to the map to indicate the center used for searching.
+        centerMarker = new atlas.HtmlMarker({
+            htmlContent: '<div class="mapCenterIcon"></div>',
+            position: map.getCamera().center
+        });
+
+        map.markers.add(centerMarker);
+
+        //Create a data source and add it to the map and enable clustering.
+        datasource = new atlas.source.DataSource(null, {
+            cluster: true,
+            clusterMaxZoom: maxClusterZoomLevel - 1
+        });
+
+        map.sources.add(datasource);
+
+        //Load all the store data now that the data source has been defined. 
+        loadStoreData();
+
+        //Create a bubble layer for rendering clustered data points.
+        var clusterBubbleLayer = new atlas.layer.BubbleLayer(datasource, null, {
+            radius: 12,
+            color: '#007faa',
+            strokeColor: 'white',
+            strokeWidth: 2,
+            filter: ['has', 'point_count'] //Only render data points which have a point_count property, which clusters do.
+        });
+
+        //Create a symbol layer to render the count of locations in a cluster.
+        var clusterLabelLayer = new atlas.layer.SymbolLayer(datasource, null, {
+            iconOptions: {
+                image: 'none' //Hide the icon image.
+            },
+            textOptions: {
+                textField: ['get', 'point_count_abbreviated'],
+                size: 12,
+                font: ['StandardFont-Bold'],
+                offset: [0, 0.4],
+                color: 'white'
+            }
+        });
+
+        map.layers.add([clusterBubbleLayer, clusterLabelLayer]);
+
+        //Load a custom image icon into the map resources.
+        map.imageSprite.add('myCustomIcon', iconImageUrl).then(function () {
+
+            //Create a layer to render a coffe cup symbol above each bubble for an individual location.
+            iconLayer = new atlas.layer.SymbolLayer(datasource, null, {
+                iconOptions: {
+                    //Pass in the id of the custom icon that was loaded into the map resources.
+                    image: 'myCustomIcon',
+
+                    //Optionally scale the size of the icon.
+                    font: ['SegoeUi-Bold'],
+
+                    //Anchor the center of the icon image to the coordinate.
+                    anchor: 'center',
+
+                    //Allow the icons to overlap.
+                    allowOverlap: true
+                },
+                filter: ['!', ['has', 'point_count']] //Filter out clustered points from this layer.
+            });
+
+            map.layers.add(iconLayer);
+
+            //When the mouse is over the cluster and icon layers, change the cursor to be a pointer.
+            map.events.add('mouseover', [clusterBubbleLayer, iconLayer], function () {
+                map.getCanvasContainer().style.cursor = 'pointer';
+            });
+
+            //When the mouse leaves the item on the cluster and icon layers, change the cursor back to the default which is grab.
+            map.events.add('mouseout', [clusterBubbleLayer, iconLayer], function () {
+                map.getCanvasContainer().style.cursor = 'grab';
+            });
+
+            //Add a click event to the cluster layer. When someone clicks on a cluster, zoom into it by 2 levels. 
+            map.events.add('click', clusterBubbleLayer, function (e) {
+                map.setCamera({
+                    center: e.position,
+                    zoom: map.getCamera().zoom + 2
+                });
+            });
+
+            //Add a click event to the icon layer and show the shape that was clicked.
+            map.events.add('click', iconLayer, function (e) {
+                showPopup(e.shapes[0]);
+            });
+
+            //Add an event to monitor when the map has finished moving.
+            map.events.add('render', function () {
+                //Give the map a chance to move and render data before updating the list.
+                updateListItems();
+            });
+        });
 
     });
 }
@@ -116,7 +214,7 @@ function setMapToUserLocation() {
 window.onload = initialize;
 
 //--------------------------------------------------------------------
-
+/*
 //Add a zoom control to the map.
 map.controls.add(new atlas.control.ZoomControl(), {
     position: 'top-right'
@@ -224,7 +322,7 @@ map.imageSprite.add('myCustomIcon', iconImageUrl).then(function() {
        updateListItems();
    });
 });
-
+*/
 //-------------------------------------------------------------------------
 
 function loadStoreData() {
